b403adaaa982de63339bdf266a6e513c2c020be0
Javier Tintaya <jhosimart42@gmail.com>
2022-10-09 11:53:35 -0400

    main terminado

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,18 @@ module.exports = {
       },
       backgroundImage: {
         "main-img":
-          "url(https://images.unsplash.com/photo-1555396273-367ea4eb4db5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80)",
+          "url('https://images.unsplash.com/photo-1601090000612-05caf795fc96?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80')",
+      },
+      height: {
+        80: "20rem",
+        100: "25rem",
+      },
+      maxWidth: {
+        cien: "62.5rem",
+      },
+      minWidth: {
+        tress: "20rem",
+        cuatros: "25rem",
       },
     },
   },
